0e0ab7f63e9dac19508bae28707f2da40a55ec4a
nikBaben <faymen44@mail.ru>
2025-10-18 22:25:16 +0300

    небольшие правки

--- app/models/robot.py
+++ app/models/robot.py
@@ -1,4 +1,3 @@
-# app/models/robot.py
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Integer, String, DateTime, func
 from datetime import datetime
@@ -10,13 +9,12 @@ class Robot(Base):
 
     id: Mapped[str] = mapped_column(String, primary_key=True)
     status: Mapped[str] = mapped_column(String)
-    battery_level: Mapped[int] = mapped_column(Integer, default=0)  # ok
+    battery_level: Mapped[int] = mapped_column(Integer, default=0)  
     last_update: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     current_zone: Mapped[str] = mapped_column(String)
     current_row: Mapped[int] = mapped_column(Integer, default=0)
     current_shelf: Mapped[int] = mapped_column(Integer, default=0)
 
-    # <-- ВАЖНО: дефолт на стороне БД и NOT NULL
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=func.now(),
